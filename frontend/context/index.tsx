e66a2250a0fb8a47859586ee8d84404cd0ec4444
shittuisrael004 <shittuisrael004@gmail.com>
2025-12-30 01:27:21 +0100

    index in context folder

---
+++ frontend/context/index.tsx
@@ -0,0 +1,49 @@
+'use client'
+
+import { wagmiAdapter, projectId } from "@/config"
+import { createAppKit } from "@reown/appkit"
+import { mainnet, arbitrum } from "@reown/appkit/networks"
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import React, {type ReactNode} from "react"
+import { cookieToInitialState, WagmiProvider, type Config } from "wagmi"
+
+const queryClient = new QueryClient()
+
+if (!projectId) {
+    throw new Error('Project Id id not defined')
+}
+
+const metadata = {
+    name: "sargesmith nft",
+    description: "collection of sargesmith nfts",
+    url: "https://github.com/shittuisrael004",
+    icons: ["https://avatars.githubusercontent.com/u/37784886"]
+}
+
+const modal = createAppKit({
+    adapters: [wagmiAdapter],
+    projectId,
+    networks: [mainnet, arbitrum],
+    defaultNetwork: mainnet,
+    features: {
+        analytics: true,
+        email: true,
+        socials: ['google', 'x', 'discord', 'farcaster'],
+        emailShowWallets: true
+    },
+    themeMode: 'light'
+})
+
+
+function ContextProvider({ children, cookies }: { children: ReactNode; cookies: string | null }) {
+  const initialState = cookieToInitialState(wagmiAdapter.wagmiConfig as Config, cookies)
+
+  return (
+    <WagmiProvider config={wagmiAdapter.wagmiConfig as Config} initialState={initialState}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </WagmiProvider>
+  )
+}
+
+export default ContextProvider
